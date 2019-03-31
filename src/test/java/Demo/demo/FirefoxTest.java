499aadb38d0640269c296a41e3a20a97283ada40
rahulpargaonkar <rahulpargaonkar1@gmail.com>
2019-03-31 07:34:13 +0530

    test

--- src/test/java/Demo/demo/FirefoxTest.java
+++ src/test/java/Demo/demo/FirefoxTest.java
@@ -2,6 +2,8 @@ package Demo.demo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
 public class FirefoxTest {
@@ -10,9 +12,14 @@ public class FirefoxTest {
 		
 		@Test
 		public void FFTest() {
+			DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+			capabilities.setCapability("marionette", true);
+			FirefoxOptions firefoxOptions = new FirefoxOptions(capabilities);
+			firefoxOptions.addArguments("--width=1920");
+			firefoxOptions.addArguments("--height=1080");
 			System.setProperty("webdriver.gecko.driver","/Users/in-rahulpa/eclipse-workspace/demo/src/test/java/binaries/geckodriver");
 			 
-		WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new FirefoxDriver(firefoxOptions);
 		driver.get("http://www.google.com");
 		}
 	
